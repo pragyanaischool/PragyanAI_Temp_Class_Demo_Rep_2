1b0d72cbe03ee5ddec71c94586b0cd66d5c81e28
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-03-29 16:52:42 +0530

    Update App.py

--- App.py
+++ App.py
@@ -115,13 +115,17 @@ elif page == "Session State":
     if "count" not in st.session_state:
         st.session_state.count = 0
 
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
     with col1:
         if st.button("Increase"):
             st.session_state.count += 1
-
+    
     with col2:
+        if st.button("Descrese"):
+            st.session_state.count -= 1
+            
+    with col3:
         if st.button("Reset"):
             st.session_state.count = 0
 
